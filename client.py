977f2580573a83c57e0c39083df57aa22f7a7610
Leila Alpieva <119100623+lyalia123@users.noreply.github.com>
2025-12-29 20:44:56 +0500

    Create client.py

---
+++ client.py
@@ -0,0 +1,39 @@
+import socket
+import json
+import uuid
+import time
+
+SERVER_IP = "<PUBLIC_IP_СЕРВЕРА>"
+PORT = 5000
+TIMEOUT = 2
+RETRIES = 3
+
+request = {
+    "request_id": str(uuid.uuid4()),
+    "method": "add",
+    "params": {"a": 5, "b": 7}
+}
+
+for attempt in range(RETRIES):
+    try:
+        print(f"Attempt {attempt + 1}")
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.settimeout(TIMEOUT)
+
+        client.connect((SERVER_IP, PORT))
+        client.send(json.dumps(request).encode())
+
+        response = client.recv(1024).decode()
+        response = json.loads(response)
+
+        print("Response:", response)
+        client.close()
+        break
+
+    except socket.timeout:
+        print("Timeout occurred, retrying...")
+    except Exception as e:
+        print("Error:", e)
+
+else:
+    print("RPC failed after retries")
